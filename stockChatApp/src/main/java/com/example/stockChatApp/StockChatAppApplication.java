96733141bf36f01318bda8f16c6177932b141fe6
Leo Lo <leolo@Leos-Mac-mini.local>
2022-05-28 10:28:27 +0800

    first commit

---
+++ stockChatApp/src/main/java/com/example/stockChatApp/StockChatAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.stockChatApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockChatAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockChatAppApplication.class, args);
+	}
+
+}
---
+++ stockChatApp/src/main/java/com/example/stockChatApp/controller/model/Status.java
@@ -0,0 +1,7 @@
+package com.example.stockChatApp.controller.model;
+
+public enum Status {
+    JOIN,
+    MESSAGE,
+    LEAVE
+}
